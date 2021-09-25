35ee4b063b0297333acf2dee29f13de8ac210b37
kenichi-inoue <ken1.inoue13@gmail.com>
2021-09-25 17:52:03 +0900

    投稿機能を実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -4,11 +4,39 @@ class PrototypesController < ApplicationController
   end
 
   def new
-    # @tweet = Tweet.new
+    @prototype = Prototype.new
   end
 
+  # def create
+  #   # @tweet = Tweet.new
+  # end
+
   def create
-    # @tweet = Tweet.new
+    # Prototype.create(prototype_params)
+    # @user = User.find(params[:user_id])
+    # @prototype = @user.prototype.new(prototype_params)
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save 
+      redirect_to root_path(@user)
+    else
+      render :new
+    end
   end
-  
+
+  private  # private以下の記述はすべてプライベートメソッドになる
+
+  def prototype_params
+    # params.require(:prototype).permit(:title,:catch_copy,:concept,image).marge(user_id current_user.id)
+    # params.require(:prototype).permit(:title,:catch_copy,:concept).marge(user_id current_user.id)
+    params.require(:prototype).permit(:title,:catch_copy,:concept,:image).merge(user_id: current_user.id)
+  end
+
+  def private_method  # プライベートメソッド
+    # （処理）
+  end
+
+  def any_method  # プライベートメソッド
+    # （処理）
+  end
+
 end
