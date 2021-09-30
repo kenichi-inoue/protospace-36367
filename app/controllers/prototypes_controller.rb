7bfcf120a19162b89e3fe096d3291439ecb05042
kenichi-inoue <ken1.inoue13@gmail.com>
2021-09-30 23:13:23 +0900

    編集機能を実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -30,6 +30,20 @@ class PrototypesController < ApplicationController
     # @prototype = Prototype.find(prototype_params)
   end
 
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
+
+
   private  # private以下の記述はすべてプライベートメソッドになる
 
   def prototype_params
