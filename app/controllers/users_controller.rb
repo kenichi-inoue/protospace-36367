e46c499d9b474e66f5c65cb979c9ca9a1b125085
kenichi-inoue <ken1.inoue13@gmail.com>
2021-10-03 22:12:39 +0900

    ユーザー名からユーザー詳細ページに遷移
    各ページのユーザー名をクリックすると、ユーザー詳細ページに遷移

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
-    # @prototype = Prototype.find(params[:id])
+    @name = current_user.name
+    # @prototype = current_user.prototype
+    # @user = User.find(params[:id])
     # @comment = Comment.new
     # @comments = @prototype.comments.includes(:user)
   end
