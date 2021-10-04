96e57a782af6621e1a0ea925c812aa988450ed3f
kenichi-inoue <ken1.inoue13@gmail.com>
2021-10-04 21:31:23 +0900

    ユーザー詳細ページでユーザーの情報が表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
   def show
-    @name = current_user.name
+    # @name = current_user.name
     # @prototype = current_user.prototype
-    # @user = User.find(params[:id])
+    @user = User.find(params[:id])
     # @comment = Comment.new
     # @comments = @prototype.comments.includes(:user)
+    # @nickname = current_user.nickname
+    # @tweets = current_user.tweets
+    @prototype = @user.prototypes
+    # @tweets = current_user.tweets
   end
 end
